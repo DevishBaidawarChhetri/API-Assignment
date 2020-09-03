83044cded4b043412de8c907dd6e3500e827c292
Rohan-shakya <rohanshakya254@gmail.com>
2020-09-03 14:13:44 +0545

    added frontend for viewing datas from api

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const express = require('express');
 const app = express();
 const PORT = 3090;
 
-//Request
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// Request
+app.get('/', (req, res) => res.redirect('home'));
+
+app.get('/home', (req, res) => {
+  res.render('home', { title: 'Softwarica College of IT & E-commerce' });
+});
+
 app.get('/api/about', callBack);
 middleware(app);
+
+app.use((req, res) => res.redirect('home'));
 
 // Listener
 app.listen(PORT, () => `Listening to port: ${PORT}`);
